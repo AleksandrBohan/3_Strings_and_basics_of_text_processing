194d45606f93a8e3635d4df7073abfc621c40131
Aleksandr Bohan <Alex_Boh@epam.com>
2021-05-19 18:02:42 +0300

    I have ended task3 from this part.

--- String operations/src/ArrayOfSymbols.java
+++ String operations/src/ArrayOfSymbols.java
@@ -1,4 +1,19 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ArrayOfSymbols {
+
+    private void task3(){
+        int count = 0;
+        String lineWithNumbers = "jnsivn 245 3ridokdsj 96bk dmklkd";
+        String template = "[0-9]";
+        Pattern pattern = Pattern.compile("[0-9]");
+        Matcher matcher = pattern.matcher(lineWithNumbers);
+        while (matcher.find()){
+            count++;
+        }
+        System.out.println(count);
+    }
 
     private void task2(){
         String worldLine = "Java is a perfect world language in the world.";
@@ -14,12 +29,13 @@ public class ArrayOfSymbols {
         String phrase = "camelCase".replaceAll(po, po);
 
         System.out.println(phrase);
-
+//TODO
     }
 
     public static void main(String[] args) {
         ArrayOfSymbols arrayOfSymbols = new ArrayOfSymbols();
         arrayOfSymbols.task1();
         arrayOfSymbols.task2();
+        arrayOfSymbols.task3();
     }
 }
