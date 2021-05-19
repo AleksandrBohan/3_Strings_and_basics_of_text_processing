d8a8bc534d0f99fdbc661053bf5d02c95c82e92e
Aleksandr Bohan <Alex_Boh@epam.com>
2021-05-19 18:19:57 +0300

    I have ended task4 from this part.

--- String operations/src/ArrayOfSymbols.java
+++ String operations/src/ArrayOfSymbols.java
@@ -3,9 +3,21 @@ import java.util.regex.Pattern;
 
 public class ArrayOfSymbols {
 
+    private void task4(){
+        int count = 0;
+        String lineWithNumbers = "jns798988ivn 2796868 87657645 3ridokdsj 96bk dmklkd";
+        String template = "\\s[0-9]+\\s";
+        Pattern pattern = Pattern.compile("\\s?[0-9]+\\s?");
+        Matcher matcher = pattern.matcher(lineWithNumbers);
+        while (matcher.find()){
+            count++;
+        }
+        System.out.println(count);
+    }
+
     private void task3(){
         int count = 0;
-        String lineWithNumbers = "jnsivn 245 3ridokdsj 96bk dmklkd";
+        String lineWithNumbers = "jnsivn 2 8  3 ridokdsj 96bk dmklkd";
         String template = "[0-9]";
         Pattern pattern = Pattern.compile("[0-9]");
         Matcher matcher = pattern.matcher(lineWithNumbers);
@@ -37,5 +49,6 @@ public class ArrayOfSymbols {
         arrayOfSymbols.task1();
         arrayOfSymbols.task2();
         arrayOfSymbols.task3();
+        arrayOfSymbols.task4();
     }
 }
