755c401fcd8ed66a1124f0b043a5a3e1d21dfea5
Aleksandr Bohan <Alex_Boh@epam.com>
2021-05-19 17:40:18 +0300

    I have ended task2 from this part.

--- String operations/src/ArrayOfSymbols.java
+++ String operations/src/ArrayOfSymbols.java
@@ -1,4 +1,14 @@
 public class ArrayOfSymbols {
+
+    private void task2(){
+        String worldLine = "Java is a perfect world language in the world.";
+        String afterChange = worldLine.replaceAll("world", "letter");
+
+        System.out.println(worldLine);
+        System.out.println(afterChange);
+
+    }
+
     private void task1(){
         String po = "[A-Z]";
         String phrase = "camelCase".replaceAll(po, po);
@@ -10,5 +20,6 @@ public class ArrayOfSymbols {
     public static void main(String[] args) {
         ArrayOfSymbols arrayOfSymbols = new ArrayOfSymbols();
         arrayOfSymbols.task1();
+        arrayOfSymbols.task2();
     }
 }
