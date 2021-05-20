1bf9ade42bf486c1bb875be9299db240c1ffc243
Aleksandr Bohan <Alex_Boh@epam.com>
2021-05-20 10:57:51 +0300

    I haven't fixed task2 from previous part. Also I haven't done task1 from this part.

--- String operations/src/ArrayOfSymbols.java
+++ String operations/src/ArrayOfSymbols.java
@@ -16,6 +16,7 @@ public class ArrayOfSymbols {
 
         System.out.println("Line before: " + lineWithNumbers);
         System.out.println("Line after: " + replaceWrongSpaces);
+        //TODO строку как массив использовать
     }
 
     private void task4(){
@@ -28,6 +29,7 @@ public class ArrayOfSymbols {
             count++;
         }
         System.out.println(count);
+        //TODO строку как массив использовать
     }
 
     private void task3(){
@@ -40,20 +42,53 @@ public class ArrayOfSymbols {
             count++;
         }
         System.out.println(count);
+        //TODO строку как массив использовать
     }
 
     private void task2(){
-        String worldLine = "Java is a perfect world language in the world.";
-        String afterChange = worldLine.replaceAll("world", "letter");
+        String wordWorld = "world";
+        String wordLetter = "letter";
+        int differentSize = 0;
+        String worldLine = "world is a perfect world language in the world.";
+        char [] arrayForReplace = worldLine.toCharArray();
+        for (int i = 0; i < arrayForReplace.length-6 ; i++){
+            if (arrayForReplace[i] == Character.valueOf(' ') &
+                    arrayForReplace[i+1] == Character.valueOf('w') &
+            arrayForReplace[i+2] == Character.valueOf('o') &
+            arrayForReplace[i+3] == Character.valueOf('r') &
+            arrayForReplace[i+4] == Character.valueOf('l') &
+            arrayForReplace[i+5] == Character.valueOf('d') &
+            arrayForReplace[i+6] == Character.valueOf(' ')){
+                char [] secondArrayForReplace = wordLetter.toCharArray();
+                if (wordLetter.length() > wordWorld.length() & differentSize == 0){
+
+                    differentSize = Integer.valueOf(wordLetter.length()) -
+                            Integer.valueOf(wordWorld.length());
+                    for (int j = 0; j<=differentSize; j++){
+                        secondArrayForReplace[0] += secondArrayForReplace[j];
+
+                    }
+                }
+for (int k = 0; k < wordWorld.length(); k++){
+               arrayForReplace[i] = secondArrayForReplace[k];
+                System.out.println();
+            }
+
+        }
+
+    }
+        String afterChange = String.valueOf(arrayForReplace);
+        // String afterChange = worldLine.replaceAll("world", "letter");
 
         System.out.println(worldLine);
         System.out.println(afterChange);
 
+//TODO
     }
 
     private void task1(){
         String text = " ";
-        String oneSymbol = null;
+        String oneSymbol;
         String phrase = "camelCaseOop";
         char [] arrayForReplace = phrase.toCharArray();
         for (int i = 0; i < arrayForReplace.length; i++){
--- String operations/src/LineAsAnObject.java
+++ String operations/src/LineAsAnObject.java
@@ -1,3 +1,30 @@
+import java.lang.ref.PhantomReference;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LineAsAnObject {
 
+    private void task2(){
+
+    }
+
+private void task1(){
+    int count = 0;
+    String lineWithSpaces = "  dffg   sfg     gf      ghghfhfhgfh";
+    String template = "\\s";
+    Pattern pattern = Pattern.compile("\\s");
+    Pattern pattern1 = Pattern.compile("\\S");
+    Matcher matcher = pattern.matcher(lineWithSpaces);
+    Matcher matcher1 = pattern.matcher(lineWithSpaces);
+    String formatString = "We are printing double variable (%f), string ('%s') and integer variable (%d).";
+    System.out.println(String.format(formatString, 2.3, "[0-9]", 10));
+   // System.out.println("Max count of spaces = " + count);
+    count = 0;
+//TODO
+}
+
+    public static void main(String[] args) {
+        LineAsAnObject lineAsAnObject = new LineAsAnObject();
+        lineAsAnObject.task1();
+    }
 }
