3b56f8ce5844358c75c28e0e1b607c8ca719ac6a
Aleksandr Bohan <Alex_Boh@epam.com>
2021-05-20 19:01:47 +0300

    I started a new part and something did.

--- String operations/src/WorkWithRegularExpressions.java
+++ String operations/src/WorkWithRegularExpressions.java
@@ -5,19 +5,29 @@ import java.util.regex.Pattern;
 public class WorkWithRegularExpressions {
 
     private void task2(int numberOfTeg){
-        String str = "<notes>\n " + "<note id =\"1\">\n" + "<to>Вася</to>\n" +
+        String str = "<notes>\n" + "<note id =\"1\">\n" + "<to>Вася</to>\n" +
                 "<from>Света</from>\n" + "<heading>Напоминание</heading>\n" +
                 "<body>Позвони мне завтра!</body>\n" + "</note>\n" +
                 "<note id =\"2\">\n" + "<to>Петя</to>\n" + "<from>Маша</from>\n" +
                 "<heading>Важное Напоминание</heading>\n" + "</body>\n" +
-                "</note>\n" + " </notes>\n ";
-Pattern[]patterns = new Pattern[10];
+                "</note>\n" + " </notes>\n";
+        Pattern[]patterns = new Pattern[10];
+        Pattern pat1 = Pattern.compile("<[a-z]{5}>.+</[a-z]{5}>\\n");
+        Pattern pat2 = Pattern.compile("<[a-z]{4}\\s[a-z]{2}\\s=\"\\d\">\\n.+" +
+        "\\n.+\\n+.\\n+.\\n</[a-z]{4}>\\n");
+        Pattern pat3 = Pattern.compile("<[a-z]{2}>.+</[a-z]{2}>");
+        Pattern pat4 = Pattern.compile("<[a-z]{4}>[А-Я][а-я]{4}.?</[a-z]{4}>");
+        Pattern pat5 = Pattern.compile("<[a-z]{7}>[А-Я][а-я]{10}</[a-z]{7}>");
+        Pattern pat6 = Pattern.compile("<[a-z]{4}>.+\\s[а-я]{3}\\s.+!</[a-z]{4}>");
+        Pattern pat7 = Pattern.compile("[a-z]{4}\\s[a-z]{2}\\s=\"\\d\">\\n<[a-z]>" +
+                "[А-Я].+</[a-z]{2}>\\n<[a-z]{4}>[А-Я][а-я]{3}</[a-z]{4}>\\n" +
+                "<[a-z]{7}>[А-Я].+\\s[А-Я].+</[a-z]{7}>\\n</[a-z]{4}>\\n" +
+                "</[a-z]{4}>\\n");
 
-        Pattern pattern = Pattern.compile("<[a-z]{4}>[А-Я][а-я]{4}.?</[a-z]{4}>");
-        Pattern pattern3 = Pattern.compile("<[a-z]{2}>.+</[a-z]{2}>");
-        Pattern pattern1 = Pattern.compile("</.+\\s=\"\\d\">");
-        Pattern pattern2 = Pattern.compile("[a-z]+");
-        Matcher matcher = pattern1.matcher(str);
+
+
+
+        Matcher matcher = pat7.matcher(str);
        // Matcher matcher1 = pattern1.matcher(str);
 
 
